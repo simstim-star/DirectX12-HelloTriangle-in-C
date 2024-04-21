15e18915b7abb3fbb898821ff4c9a010c52ae5fe
simstim-star <raidemall01@gmail.com>
2024-04-21 16:42:13 -0300

    Remove unnecessary word

--- window.h
+++ window.h
@@ -6,4 +6,4 @@ extern HWND G_HWND;
 
 typedef struct DXSample DXSample;
 
-int Win32App_Run(struct DXSample* const pSample, const HINSTANCE hInstance, const int nCmdShow);
+int Win32App_Run(DXSample* const pSample, const HINSTANCE hInstance, const int nCmdShow);
